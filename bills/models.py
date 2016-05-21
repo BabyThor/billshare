e0edfbce9f08fdec3fa04da81e269c980f329064
BabyThor <w.wangtrakoon@gmail.com>
2016-05-21 22:47:58 +0700

    Change field name for model Bill and Item to not allow blank value

--- bills/models.py
+++ bills/models.py
@@ -3,12 +3,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Bill(models.Model):
-	name = models.CharField(max_length=200)
+	name = models.CharField(max_length=200, null=False, blank=False)
 	status = models.BooleanField(default=False)
 	date = models.DateTimeField()
 
 class Item(models.Model):
 	bill = models.ForeignKey('bills.Bill')
-	name = models.CharField(max_length=200)
+	name = models.CharField(max_length=200, null=False, blank=False)
 	amount = models.IntegerField()
 	price = models.IntegerField()
