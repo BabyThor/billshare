ae265c3d6b1cdb03707bbc4cab28e1244eb44cdd
BabyThor <w.wangtrakoon@gmail.com>
2016-05-21 22:38:26 +0700

    Make relation for item to bill

--- bills/models.py
+++ bills/models.py
@@ -2,11 +2,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-class Item(models.Model):
-	name = models.CharField(max_length=200)
-	amount = models.IntegerField()
-	price = models.IntegerField()
-
 class Bill(models.Model):
 	name = models.CharField(max_length=200)
 	status = models.BooleanField(default=False)
+
+class Item(models.Model):
+	bill = models.ForeignKey('bills.Bill')
+	name = models.CharField(max_length=200)
+	amount = models.IntegerField()
+	price = models.IntegerField()
