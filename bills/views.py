3aa3d2191d04bbdc754548886a81da6ca6b59256
BabyThor <w.wangtrakoon@gmail.com>
2016-05-22 18:40:18 +0700

    Create home view

--- bills/views.py
+++ bills/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+from django.conf import settings
+
+class Home(TemplateView):
+    template_name = 'home.html'
+
+    def get(self, request):
+        print settings.TEMPLATE_DIRS
+        return render(
+            request,
+            self.template_name
+        )
