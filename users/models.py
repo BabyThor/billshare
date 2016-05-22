aacad4db7433183a44735f3288e25d972e9df18b
BabyThor <w.wangtrakoon@gmail.com>
2016-05-22 11:22:28 +0700

    Create Friend model

--- users/models.py
+++ users/models.py
@@ -2,4 +2,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Friend(models.Model):
+	bill = models.ForeignKey('bills.Bill')
+	name = models.CharField(max_length=200, null=False, blank=False)
+	paid = models.IntegerField()
+	items = models.ManyToManyField('bills.Item')
