f1df8171508d5d903ebee7972c1b595ab4f6d684
BabyThor <w.wangtrakoon@gmail.com>
2016-05-30 23:37:25 +0700

    Make Bill and Item model readable

--- bills/models.py
+++ bills/models.py
@@ -7,8 +7,14 @@ class Bill(models.Model):
 	status = models.BooleanField(default=False)
 	date = models.DateTimeField()
 
+	def __unicode__(self):
+		return self.name
+
 class Item(models.Model):
 	bill = models.ForeignKey('bills.Bill')
 	name = models.CharField(max_length=200, null=False, blank=False)
 	amount = models.IntegerField()
 	price = models.IntegerField()
+
+	def __unicode__(self):
+		return '%s (%s)' % (self.name, self.bill.name)
