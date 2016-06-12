370fe10c9a0c943a446114dff015250981711e18
BabyThor <w.wangtrakoon@gmail.com>
2016-06-13 00:34:57 +0700

    Add default value for paid in Friend model

--- users/models.py
+++ users/models.py
@@ -5,5 +5,5 @@ from django.db import models
 class Friend(models.Model):
 	bill = models.ForeignKey('bills.Bill')
 	name = models.CharField(max_length=200, null=False, blank=False)
-	paid = models.IntegerField()
+	paid = models.IntegerField(default=0)
 	items = models.ManyToManyField('bills.Item')
