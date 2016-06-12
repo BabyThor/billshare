1a78badd7cc252c73cb914766e4442b34b561d3f
BabyThor <w.wangtrakoon@gmail.com>
2016-06-12 21:05:02 +0700

    Make Host view can create bill and item on post method

--- bills/views.py
+++ bills/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.utils import timezone
 
 from django.conf import settings
 from .forms import HostForm
+from .models import Bill, Item
 
 class Home(TemplateView):
     template_name = 'home.html'
@@ -15,26 +17,35 @@ class Home(TemplateView):
 
 
 class Host(TemplateView):
-	form = HostForm
-	template_name = 'host.html'
+    form = HostForm
+    template_name = 'host.html'
 
-	def get(self, request):
-		return render(
-			request,
-			self.template_name,
-			{
-				'form': self.form,
-			}
-		)
+    def get(self, request):
+        return render(
+            request,
+            self.template_name,
+            {
+                'form': self.form,
+            }
+        )
 
-	def post(self, request):
-		form = self.form(request.POST)
-		if form.is_valid():
-			form.save()
-		return render(
-			request,
-			self.template_name,
-			{
-				'form': self.form,
-			}
-		)
+    def post(self, request):
+        bill_name = request.POST.get('bill_name')
+        list_item_name = request.POST.getlist('item_name')
+        list_item_amount = request.POST.getlist('item_amount')
+        list_item_price = request.POST.getlist('item_price')
+        bill = Bill.objects.create(name=bill_name, date=timezone.now())
+        for index in range(len(list_item_name)):
+            Item.objects.create(
+                bill=bill,
+                name=list_item_name[index],
+                amount=list_item_amount[index],
+                price=list_item_price[index]
+            )
+        return render(
+            request,
+            self.template_name,
+            {
+                'form': self.form,
+            }
+        )
